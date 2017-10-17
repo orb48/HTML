e315a4c3844d333f64e3b49c26d93a416f2691b4
orb48 <olga.bovkun.ob@gmail.com>
2017-10-17 23:50:05 +0700

    Gabriel

--- Gabriel/js/script.js
+++ Gabriel/js/script.js
@@ -1,24 +1,29 @@
 $(document).ready(function() {
-	var sliders = $("td.request-col #slider");
+	var sliders = $("td.request-col .slider");
 	var inputs = $("td.value-slider-col .value");
 	sliders.each(function() {
-		$(this).empty().slider({
+		$(this).slider({
 			min: 0,
 			max: 1000,
 			step: 100,
 			slide: function(event, ui){
-				$("input.value").val(ui.value);
+				$(this).closest("tr").find("input.value").val(ui.value);
 			},
 			stop: function(event, ui){
-				$("input.value").val(ui.value);
+				$(this).closest("tr").find("input.value").val(ui.value);
 			}
 		});
 
 		inputs.each(function(index) {
 			$(this).change(function(){
 				var value = $(this).val();
-				$(this).val(value);
-				$("#slider").slider("value", value);
+				if(value%100 === 0) {
+					$(this).val(value);
+					$(this).closest("tr").find(".slider").slider("value", value);
+					changeValue = true;
+				} else {
+					$(this).css('border','1px solid red');
+				}
 			});
 		});
 	});
